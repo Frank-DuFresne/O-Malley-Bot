fd831d2769ea66c6eba7a5c8245c0205f8841679
Ethan <ehanlon19@gmail.com>
2019-11-20 18:35:03 -0800

    My robot code, but now with comments.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,15 +12,19 @@ import java.time.Instant;
 
 public class Robot extends TimedRobot {
 
+    //Instantiating the motor controllers
     private WPI_VictorSPX m_FrontLeft;
     private WPI_VictorSPX m_FrontRight;
     private WPI_VictorSPX m_BackLeft;
     private WPI_VictorSPX m_BackRight;
 
+    //Instantiating the drivetrain
     private DifferentialDrive m_Drivetrain;
 
+    //Instantiating the joystick
     private Joystick m_Joystick;
 
+    //Instantiating statistic variables
     private StringBuilder statisticsBuilding = new StringBuilder();
     private int statisticLooperCounter = 0;
     private SimpleDateFormat sDF = new SimpleDateFormat("MM/dd/yy HH:mm:ss");
@@ -28,30 +32,37 @@ public class Robot extends TimedRobot {
     @Override
     public void robotInit() {
 
+        //Stringbuilder for initial statistics
         public StringBuilder yeet = new StringBuilder();
 
+        //Assigning the motor controllers
         m_FrontLeft = new WPI_VictorSPX(0);
         m_FrontRight = new WPI_VictorSPX(1);
         m_BackLeft = new WPI_VictorSPX(2);
         m_BackRight = new WPI_VictorSPX(3);
 
+        //Assigning the drivetrain
         m_Drivetrain = new DifferentialDrive(m_FrontLeft, m_FrontRight);
 
+        //Assigning the joysstick
         m_Joystick = new Joystick(0);
 
+        //Restoring factory defaults on the motor controllers
         m_FrontLeft.configFactoryDefault();
         m_FrontRight.configFactoryDefault();
         m_BackLeft.configFactoryDefault();
         m_BackRight.configFactoryDefault();
 
+        //Making the back motors follow the front ones
         m_BackLeft.follow(m_FrontLeft);
         m_BackRight.follow(m_FrontRight);
 
+        //Putting the drivetrain on Shuffleboard
         Shuffleboard.getTab("Drivetrain").add(m_Drivetrain);
 
+        // Lines 64 to 72 are where the initial statistics get combined and printed
         File elif = new File(Robot.class.getProtectionDomain().getCodeSource().getLocation().getPath());
         yeet.append("Code Last Deployed: ").append(sDF.format(elif.lastModified()));
-
         yeet.append("\n----------\nFront Left Drivetrain Firmware Version: ").append(m_FrontLeft.getFirmwareVersion());
         yeet.append("\nFront Right Drivetrain Firmware Version: ").append(m_FrontRight.getFirmwareVersion());
         yeet.append("\nBack Left Drivetrain Firmware Version: ").append(m_BackLeft.getFirmwareVersion());
@@ -68,16 +79,21 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
+
+        //THE arcade drive
         m_Drivetrain.arcadeDrive(-m_Joystick.getY(),m_Joystick.getX());
 
+        //Statistic timer & runner
         statisticLooperCounter++;
-        if (statisticLooperCounter >= 9) {
+        if (statisticLooperCounter >= 10) {
             statisticPrinting();
             statisticLooperCounter = 0;
         }
     }
 
     public void statisticPrinting() {
+
+        //Statistics printed 5 times per second (once per 10 iterations)
         statisticsBuilding.append("----------");
         statisticsBuilding.append("\nTimestamp: ").append(sDF.format(Instant.now().getEpochSecond()));
         statisticsBuilding.append("\nFront Left Motor Power Level: ").append(m_FrontLeft.get());
