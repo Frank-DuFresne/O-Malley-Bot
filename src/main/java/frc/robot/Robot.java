eb82e30ca9c0465cbef1eae0c00ef914a307dd41
Ethan <ehanlon19@gmail.com>
2019-11-20 18:01:20 -0800

    Finished Drivetrain.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,8 +4,11 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 
 public class Robot extends TimedRobot {
 
@@ -25,6 +28,8 @@ public class Robot extends TimedRobot {
     @Override
     public void robotInit() {
 
+        public StringBuilder yeet = new StringBuilder();
+
         m_FrontLeft = new WPI_VictorSPX(0);
         m_FrontRight = new WPI_VictorSPX(1);
         m_BackLeft = new WPI_VictorSPX(2);
@@ -34,5 +39,53 @@ public class Robot extends TimedRobot {
 
         m_Joystick = new Joystick(0);
 
+        m_FrontLeft.configFactoryDefault();
+        m_FrontRight.configFactoryDefault();
+        m_BackLeft.configFactoryDefault();
+        m_BackRight.configFactoryDefault();
+
+        m_BackLeft.follow(m_FrontLeft);
+        m_BackRight.follow(m_FrontRight);
+
+        Shuffleboard.getTab("Drivetrain").add(m_Drivetrain);
+
+        File elif = new File(Robot.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+        yeet.append("Code Last Deployed: ").append(sDF.format(elif.lastModified()));
+
+        yeet.append("\n----------\nFront Left Drivetrain Firmware Version: ").append(m_FrontLeft.getFirmwareVersion());
+        yeet.append("\nFront Right Drivetrain Firmware Version: ").append(m_FrontRight.getFirmwareVersion());
+        yeet.append("\nBack Left Drivetrain Firmware Version: ").append(m_BackLeft.getFirmwareVersion());
+        yeet.append("\nBack Right Drivetrain Firmware Version: ").append(m_BackRight.getFirmwareVersion());
+        yeet.append("\n----------\n");
+        System.out.println(yeet);
+        yeet.setLength(0);
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        teleopPeriodic();
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        m_Drivetrain.arcadeDrive(-m_Joystick.getY(),m_Joystick.getX());
+
+        statisticLooperCounter++;
+        if (statisticLooperCounter >= 9) {
+            statisticPrinting();
+            statisticLooperCounter = 0;
+        }
+    }
+
+    public void statisticPrinting() {
+        statisticsBuilding.append("----------");
+        statisticsBuilding.append("\nTimestamp: ").append(sDF.format(Instant.now().getEpochSecond()));
+        statisticsBuilding.append("\nFront Left Motor Power Level: ").append(m_FrontLeft.get());
+        statisticsBuilding.append("\nFront Right Motor Power Level: ").append(m_FrontRight.get());
+        statisticsBuilding.append("\nBack Left Motor Power Level: ").append(m_BackLeft.get());
+        statisticsBuilding.append("\nBack Right Motor Power Level: ").append(m_BackRight.get());
+        statisticsBuilding.append("----------");
+        System.out.println(statisticsBuilding);
+        statisticsBuilding.setLength(0);
     }
 }
